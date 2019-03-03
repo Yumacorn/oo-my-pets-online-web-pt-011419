94eab3b8a98e4159c3b97bd396dbd676a190e6e5
Ryan Mui <reachryanmui@gmail.com>
2019-03-03 03:35:39 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,5 +1,33 @@
 class Owner
   # code goes here
+  attr_accessor :name, :pets
   attr_reader :species
+  @@all = []
+  @@count = 0
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    @@count += 1
+    @species = "human"
+    @pets = {fishes: [], dogs: [], cats: []}
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.reset_all
+    @@all = []
+    @@count = 0
+  end
 
 end
